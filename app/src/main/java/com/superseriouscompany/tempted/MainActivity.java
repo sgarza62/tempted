e48b2200f681719cf23cf3102b8bc6c7a3cb2523
Santiago Garza <sgarza62@yahoo.com>
2016-06-08 09:26:12 -0700

    Add mosquito squash and resurrection

--- app/src/main/java/com/superseriouscompany/tempted/MainActivity.java
+++ app/src/main/java/com/superseriouscompany/tempted/MainActivity.java
@@ -1,13 +1,37 @@
 package com.superseriouscompany.tempted;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView mLiveMosquito;
+    private ImageView mDeadMosquito;
+    private Handler mDurationHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLiveMosquito = (ImageView) findViewById(R.id.liveMosquitoImg);
+        mDeadMosquito = (ImageView) findViewById(R.id.deadMosquitoImg);
     }
+
+    public void squash(View view) {
+        mLiveMosquito.setVisibility(View.GONE);
+        mDeadMosquito.setVisibility(View.VISIBLE);
+        mDurationHandler.postDelayed(resurrectMosquitoRunnable, 3000);
+    }
+
+    protected Runnable resurrectMosquitoRunnable = new Runnable() {
+        @Override
+        public void run() {
+            mDeadMosquito.setVisibility(View.GONE);
+            mLiveMosquito.setVisibility(View.VISIBLE);
+        }
+    };
 }
